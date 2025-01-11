090f2bc72aba34db4c88692251f98d57cdbcd88f
O.Gogol <o.gogol91@gmail.com>
2025-01-11 17:49:24 +0000

    failing version

--- src/test/java/ru/netology/rest/MobileBankApiTestV3.java
+++ src/test/java/ru/netology/rest/MobileBankApiTestV3.java
@@ -19,12 +19,12 @@ class MobileBankApiTestV3 {
                 .get("/demo/accounts")
                 // Проверки
                 .then()
-                .statusCode(2000)
+                .statusCode(200)
 
         // специализированные проверки - лучше
                 .contentType(ContentType.JSON)
                 .body("", hasSize(3))
-                .body("[0].currency", equalTo("RUB"))
+                .body("[0].currency", equalTo("RUR"))
                 .body("[1].currency", equalTo("USD"))
                 .body("[0].balance", greaterThanOrEqualTo(0))
                 .body(matchesJsonSchemaInClasspath("accounts.schema.json"));
