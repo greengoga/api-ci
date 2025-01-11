034361bdad885baa31b89bf25e6b93cab97d85cc
O.Gogol <o.gogol91@gmail.com>
2025-01-11 11:32:24 +0000

    failing run (RUR)

--- src/test/java/ru/netology/rest/MobileBankApiTestV3.java
+++ src/test/java/ru/netology/rest/MobileBankApiTestV3.java
@@ -25,7 +25,7 @@ class MobileBankApiTestV3 {
                 .contentType(ContentType.JSON)
 
                 .body("", hasSize(3))
-                .body("[0].currency", equalTo("RUB"))
+                .body("[0].currency", equalTo("RUR"))
                 .body("[1].currency", equalTo("USD"))
                 .body("[0].balance", greaterThanOrEqualTo(0))
                 .body(matchesJsonSchemaInClasspath("accounts.schema.json"));
