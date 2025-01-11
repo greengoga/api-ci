e2445576776869e0d9b359c736cea0db379d4239
O.Gogol <o.gogol91@gmail.com>
2025-01-11 11:31:17 +0000

    successful run

--- src/test/java/ru/netology/rest/MobileBankApiTestV3.java
+++ src/test/java/ru/netology/rest/MobileBankApiTestV3.java
@@ -4,6 +4,7 @@ import io.restassured.http.ContentType;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
+import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
 import static org.hamcrest.Matchers.*;
 
 class MobileBankApiTestV3 {
@@ -19,12 +20,16 @@ class MobileBankApiTestV3 {
                 // Проверки
                 .then()
                 .statusCode(200)
-                // специализированные проверки - лучше
+
+        // специализированные проверки - лучше
                 .contentType(ContentType.JSON)
+
                 .body("", hasSize(3))
                 .body("[0].currency", equalTo("RUB"))
                 .body("[1].currency", equalTo("USD"))
                 .body("[0].balance", greaterThanOrEqualTo(0))
+                .body(matchesJsonSchemaInClasspath("accounts.schema.json"));
+
         ;
     }
 }
